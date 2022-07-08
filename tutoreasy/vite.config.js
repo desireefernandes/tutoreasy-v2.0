07484f99370acea51c0949ea01b842817f1b2b27
Rayaninha <salesrayane11@gmail.com>
2022-07-08 10:50:59 -0300

    Revert "Estrutura inicial com sistema de login #5"
    This reverts commit 3fac8bbe6b285e537fd71af822070f0c4f98d460.

--- tutoreasy/vite.config.js
+++ tutoreasy/vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
+        laravel([
+            'resources/css/app.css',
+            'resources/js/app.js',
+        ]),
     ],
 });
