06f275f513706c2300accb91b708eb0d853ab0f0
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2022-10-20 22:14:10 -0300

    Importando fontes e configurando o Storybook.

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,7 +3,11 @@ import './styles/global.css';
 export function App() {
 
   return (
-    <h1>TutorEasy</h1>
+    <>
+      <h1 className="font-bold text-5xl text-violet-500">TutorEasy</h1>
+
+      <button className="bg-violet-500 font-mediun px-4 py2 rounded text-white houver:bg-violet-680">Enviar</button>
+    </>
 
   )
 }
